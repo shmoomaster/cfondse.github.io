fa6843c9b65e782030abd0c49e907a87b0b5cc11
shmoomaster <163171927+shmoomaster@users.noreply.github.com>
2024-04-15 17:41:35 -0700

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -25,7 +25,7 @@ const CONFIG = {
       },
       manual: {
         // Properties for manually specifying projects
-        projects: ['shmoomaster'Bioinfo, 'shmoomaster/Bioinformatics'], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
+        projects: ['shmoomaster/Bioinfo', 'shmoomaster/Bioinformatics'], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
       },
     },
     external: {
